e8892d41f5c55925bd1fa2e0311a75e9afec686d
Dylan Musil <dylanmusil@gmail.com>
2022-08-14 16:13:38 -0400

    Integration test

---
+++ Tests/WebTest.cs
@@ -0,0 +1,23 @@
+﻿using BuildingBlocks;
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace Tests;
+
+public class WebTest
+{
+    private HttpClient _client;
+
+    [SetUp]
+    public void Setup()
+    {
+        var factory = new WebApplicationFactory<Program>();
+        _client = factory.CreateClient();
+    }
+
+    [Test]
+    public async Task Can_call_API()
+    {
+        var response = await _client.GetAsync("/WeatherForecast");
+        Assert.That(response.IsSuccessStatusCode);
+    }
+}

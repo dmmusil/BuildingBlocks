74c29e7dbe625af59fb62c804ec7ca44b43f9db9
Dylan Musil <dylanmusil@gmail.com>
2022-08-14 14:03:49 -0400

    Fix rounding

--- Tests/WeatherForecastExampleTest.cs
+++ Tests/WeatherForecastExampleTest.cs
@@ -6,9 +6,16 @@ public class WeatherForecastExampleTest
 {
 
     [Test]
-    public void Test1()
+    public void Zero_C_is_32_F()
     {
         var sut = new WeatherForecast { TemperatureC = 0 };
         Assert.That(sut.TemperatureF, Is.EqualTo(32));
+    }
+
+    [Test]
+    public void Boiling_Is_212_F()
+    {
+        var sut = new WeatherForecast { TemperatureC = 100 };
+        Assert.That(sut.TemperatureF, Is.EqualTo(212));
     }
 }
